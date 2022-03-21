59f168cfb259bfec44ca0fae373dd75a40f4e52e
David Webb <dave@davidwebb.me.uk>
2022-03-21 08:36:37 +0000

    :rotating_light: Fix pylint warnings
    1. Add pylintrc config file
    2. Fix some warnings

--- bobcat.py
+++ bobcat.py
@@ -1,14 +1,15 @@
+"""Convert BBC Sounds subscription to an RSS Feed."""
+
 import argparse
-import atexit
 import os
-import youtube_dl
+
 import json
 import shutil
-import time
-import pytz
-import requests
 from pathlib import Path
 from datetime import datetime
+import pytz
+import requests
+import youtube_dl
 from feedgen.feed import FeedGenerator
 from mutagen.mp4 import MP4
 from selenium import webdriver
@@ -22,19 +23,26 @@ URL_BBC_LOGIN = 'https://account.bbc.com/signin'
 URL_BBC_SOUNDS = 'https://www.bbc.co.uk/sounds'
 URL_BBC_MY_SOUNDS = 'https://www.bbc.co.uk/sounds/my?page={}'
 
+DRIVER = None
+OUTPUT_DIR = None
 
 class Episode:
-    def __init__(self, url=None, id=None):
-        if id:
-            self.id = id
+    """Single of episode of a show on BBC Sounds"""
+
+    def __init__(self, url=None, episode_id=None):
+        if episode_id:
+            self.episode_id = episode_id
         else:
             self.url = url
-            self.id = url.split('/')[-1]
-        
-        self.data_directory = f'{output_dir}/{self.id}'
+            self.episode_id = url.split('/')[-1]
+
+        self.data_directory = f'{OUTPUT_DIR}/{self.episode_id}'
         self.audio_filename = f'{self.data_directory}/audio.m4a'
         self.metadata_filename = f'{self.data_directory}/episode.json'
         self.image_filename = f'{self.data_directory}/image.jpg'
+        self.title = None
+        self.description = None
+        self.image_url = None
 
 
     def is_audio_downloaded(self):
@@ -48,7 +56,7 @@ class Episode:
 
         return Path(self.image_filename).exists()
 
-    
+
     def is_downloaded(self):
         """Returns true if the audio and image is downloaded for this episode"""
 
@@ -56,10 +64,10 @@ class Episode:
 
 
     def _read_metadata_file(self):
-        with open(self.metadata_filename, 'r') as metadata:
+        with open(self.metadata_filename, encoding='utf8') as metadata:
             episode_metadata = json.loads(metadata.read())
 
-        if episode_metadata['id'] != self.id:
+        if episode_metadata['id'] != self.episode_id:
             raise ValueError()
 
         self.url = episode_metadata['url']
@@ -70,7 +78,7 @@ class Episode:
 
     def _write_metadata_file(self):
         episode_metadata = {
-            'id': self.id,
+            'id': self.episode_id,
             'url': self.url,
             'title': self.title,
             'description': self.description,
@@ -78,43 +86,39 @@ class Episode:
         }
 
         Path(self.data_directory).mkdir(parents=True, exist_ok=True)
-        with open(self.metadata_filename, 'w') as metadata:
+        with open(self.metadata_filename, mode='w', encoding='utf8') as metadata:
             metadata.write(json.dumps(episode_metadata))
 
 
     def load_metadata(self):
         """Get metadata from local cache or from website"""
 
-        global output_dir
-
         try:
             self._read_metadata_file()
-            print(f'Read metadata for {self.id} from file {self.metadata_filename}')
+            print(f'Read metadata for {self.episode_id} from file {self.metadata_filename}')
         except:
             self._fetch_metadata()
             self._write_metadata_file()
-            print(f'Read metadata for {self.id} from website')
+            print(f'Read metadata for {self.episode_id} from website')
 
 
     def _fetch_metadata(self):
         """Get information about an episode from the BBC Sounds website"""
-        
-        global driver
 
-        driver.get(self.url)
+        DRIVER.get(self.url)
 
         try:
-            show_more = driver.find_element(By.CLASS_NAME, 'sc-c-synopsis__button')
+            show_more = DRIVER.find_element(By.CLASS_NAME, 'sc-c-synopsis__button')
             show_more.click()
         except NoSuchElementException:
             pass
 
-        heading = driver.find_element(By.CSS_SELECTOR, '.sc-c-herospace__details-titles .sc-u-screenreader-only')
-        synopsis = driver.find_element(By.CLASS_NAME, 'sc-c-synopsis')
-        image = driver.find_element(By.CLASS_NAME, 'sc-c-herospace__image')
-        
+        heading = DRIVER.find_element(By.CSS_SELECTOR, '.sc-c-herospace__details-titles .sc-u-screenreader-only')
+        synopsis = DRIVER.find_element(By.CLASS_NAME, 'sc-c-synopsis')
+        image = DRIVER.find_element(By.CLASS_NAME, 'sc-c-herospace__image')
+
         title = heading.text
-        
+
         if title.endswith(' - BBC Sounds'):
             title = title[:-13]
 
@@ -125,8 +129,6 @@ class Episode:
 
 def initialise_selenium(foreground):
     """Initialise the Selenium driver"""
-    global driver
-
     chrome_options = Options()
 
     if foreground:
@@ -138,45 +140,43 @@ def initialise_selenium(foreground):
     service = Service(chromedriver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.set_window_size(1024, 1280)
+    return driver
 
 
 def clean_up_selenium():
-    global driver
-
-    driver.close()
+    """Tidy up Selenium resources"""
+    DRIVER.close()
 
 
 def bbc_login(bbc_username, bbc_password):
     """Login in to the BBC site"""
 
-    driver.get(URL_BBC_LOGIN)
-    username_field = driver.find_element(By.ID, 'user-identifier-input')
+    DRIVER.get(URL_BBC_LOGIN)
+    username_field = DRIVER.find_element(By.ID, 'user-identifier-input')
     username_field.send_keys(bbc_username)
-    password_field = driver.find_element(By.ID, 'password-input')
+    password_field = DRIVER.find_element(By.ID, 'password-input')
     password_field.send_keys(bbc_password)
-    submit_button = driver.find_element(By.ID, 'submit-button')
+    submit_button = DRIVER.find_element(By.ID, 'submit-button')
     submit_button.click()
 
 
 def get_episodes(max_episodes):
     """Get the episodes of shows subscribed to on BBC Sounds"""
 
-    global driver 
-
     episode_urls = []
     page = 0
 
     while True:
         page += 1
-        driver.get(URL_BBC_MY_SOUNDS.format(page))
+        DRIVER.get(URL_BBC_MY_SOUNDS.format(page))
 
         # Click on the accept cookies prompt if it is displayed
-        accept_cookies = driver.find_elements(By.CSS_SELECTOR, '#bbccookies-continue-button')
-    
+        accept_cookies = DRIVER.find_elements(By.CSS_SELECTOR, '#bbccookies-continue-button')
+
         if accept_cookies:
             accept_cookies[0].click()
 
-        locations = driver.find_elements(By.CSS_SELECTOR, 'div.sounds-react-app li a[href*="/play/"]')
+        locations = DRIVER.find_elements(By.CSS_SELECTOR, 'div.sounds-react-app li a[href*="/play/"]')
         page_episode_urls = [anchor.get_attribute('href') for anchor in locations]
         episode_count = len(page_episode_urls)
 
@@ -211,20 +211,20 @@ def download_episode_image(episode):
     """Download the image files for each episode"""
 
     if episode.is_image_downloaded():
-        print(f'Image for episode {episode.id} already downloaded')
+        print(f'Image for episode {episode.episode_id} already downloaded')
     else:
         response = requests.get(episode.image_url, stream=True)
         with open(episode.image_filename, 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
 
-        print(f'Image for episode {episode.id} downloaded from website')
+        print(f'Image for episode {episode.episode_id} downloaded from website')
 
 
 def download_episode_audio(episode):
     """Download audio files for each episode"""
 
     if episode.is_audio_downloaded():
-        print(f'Audio for episode {episode.id} already downloaded')
+        print(f'Audio for episode {episode.episode_id} already downloaded')
     else:
         ydl_options = {
             'outtmpl': episode.audio_filename,
@@ -232,12 +232,12 @@ def download_episode_audio(episode):
         }
 
         with youtube_dl.YoutubeDL(ydl_options) as ydl:
-            
+
             try:
                 # This automatically skips if file is already downloaded
                 ydl.download([episode.url])
-            except Exception as e:
-                print(repr(e))
+            except:
+                pass
 
 
 def load_episodes():
@@ -245,10 +245,10 @@ def load_episodes():
 
     episodes = []
 
-    for dir, _, files in os.walk(output_dir):
+    for directory, _, files in os.walk(OUTPUT_DIR):
         if 'episode.json' in files:
-            episode_id = Path(dir).relative_to(output_dir).name
-            episode = Episode(id=episode_id)
+            episode_id = Path(directory).relative_to(OUTPUT_DIR).name
+            episode = Episode(episode_id=episode_id)
             episode.load_metadata()
             episodes.append(episode)
 
@@ -256,33 +256,33 @@ def load_episodes():
 
 
 def create_rss_feed(episodes, podcast_path):
-    global output_dir
+    """Create the RSS file for the episodes"""
 
-    shutil.copy2('logo.png', output_dir)
+    shutil.copy2('logo.png', OUTPUT_DIR)
     logo_url = f'{podcast_path}/logo.png'
 
     episodes = (episode for episode in episodes if episode.is_downloaded())
 
-    fg = FeedGenerator()
-    fg.load_extension('podcast')
+    feed_generator = FeedGenerator()
+    feed_generator.load_extension('podcast')
 
-    fg.title('BBC Sounds Subscriptions')
-    fg.description('Episodes of shows I have subscribed to on BBC Sounds')
-    fg.author({'name': 'BBC Sounds', 'email': 'sounds@bbc.co.uk'})
-    fg.logo(f'{podcast_path}/logo.png')
-    fg.link(href=f'{podcast_path}/podcast.xml', rel='self')
-    fg.language('en')
+    feed_generator.title('BBC Sounds Subscriptions')
+    feed_generator.description('Episodes of shows I have subscribed to on BBC Sounds')
+    feed_generator.author({'name': 'BBC Sounds', 'email': 'sounds@bbc.co.uk'})
+    feed_generator.logo(f'{podcast_path}/logo.png')
+    feed_generator.link(href=f'{podcast_path}/podcast.xml', rel='self')
+    feed_generator.language('en')
 
-    fg.podcast.itunes_category('Arts')
-    fg.podcast.itunes_category('Comedy')
-    fg.podcast.itunes_category('Music')
-    fg.podcast.itunes_category('News')
-    fg.podcast.itunes_category('Sports')
-    fg.podcast.itunes_author('BBC Sounds')
-    fg.podcast.itunes_block(True)
-    fg.podcast.itunes_explicit('no')
-    fg.podcast.itunes_image(logo_url)
-    fg.podcast.itunes_owner(name='BBC Sounds', email='sounds@bbc.co.uk')
+    feed_generator.podcast.itunes_category('Arts')
+    feed_generator.podcast.itunes_category('Comedy')
+    feed_generator.podcast.itunes_category('Music')
+    feed_generator.podcast.itunes_category('News')
+    feed_generator.podcast.itunes_category('Sports')
+    feed_generator.podcast.itunes_author('BBC Sounds')
+    feed_generator.podcast.itunes_block(True)
+    feed_generator.podcast.itunes_explicit('no')
+    feed_generator.podcast.itunes_image(logo_url)
+    feed_generator.podcast.itunes_owner(name='BBC Sounds', email='sounds@bbc.co.uk')
 
     for episode in episodes:
         size_in_bytes = os.path.getsize(episode.audio_filename)
@@ -293,17 +293,17 @@ def create_rss_feed(episodes, podcast_path):
         audio_url = f'{podcast_path}/{episode.audio_filename}'
         image_url = f'{podcast_path}/{episode.image_filename}'
 
-        fe = fg.add_entry()
-        fe.id(audio_url)
-        fe.title(episode.title)
-        fe.description(episode.description)
-        fe.enclosure(url=audio_url, length=str(size_in_bytes), type='audio/mpeg')
-        fe.published(published)
-        fe.podcast.itunes_duration(duration_in_seconds)
-        fe.podcast.itunes_image(image_url)
+        feed_entry = feed_generator.add_entry()
+        feed_entry.id(audio_url)
+        feed_entry.title(episode.title)
+        feed_entry.description(episode.description)
+        feed_entry.enclosure(url=audio_url, length=str(size_in_bytes), type='audio/mpeg')
+        feed_entry.published(published)
+        feed_entry.podcast.itunes_duration(duration_in_seconds)
+        feed_entry.podcast.itunes_image(image_url)
 
-    fg.rss_str(pretty=True)
-    fg.rss_file(f'{output_dir}/podcast.xml')
+    feed_generator.rss_str(pretty=True)
+    feed_generator.rss_file(f'{OUTPUT_DIR}/podcast.xml')
 
 
 # def copy_to_s3(episodes):
@@ -312,7 +312,10 @@ def create_rss_feed(episodes, podcast_path):
 
 
 def main():
-    global output_dir
+    """Main"""
+
+    global DRIVER
+    global OUTPUT_DIR
 
     parser = argparse.ArgumentParser(description='Convert BBC Sounds subscription to an RSS Feed.')
     parser.add_argument('-u', '--bbc-username', required=True, help='BBC account username or email')
@@ -327,7 +330,7 @@ def main():
     bbc_username = args.bbc_username
     bbc_password = args.bbc_password
     foreground = args.show_browser
-    output_dir = args.output_dir
+    OUTPUT_DIR = args.output_dir
     podcast_path = args.podcast_path
     cache = args.cache
     max_episodes = args.max_episodes
@@ -335,7 +338,7 @@ def main():
     if cache:
         episodes = load_episodes()
     else:
-        initialise_selenium(foreground)
+        DRIVER = initialise_selenium(foreground)
         bbc_login(bbc_username, bbc_password)
         episodes = get_episodes(max_episodes)
 
