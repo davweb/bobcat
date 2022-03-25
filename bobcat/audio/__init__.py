dfb803b104ff68b4d57375103d07ac2c58bbfe8d
David Webb <dave@davidwebb.me.uk>
2022-03-25 17:59:19 +0000

    :rotating_light: Fix line to long pylint warnings

--- bobcat/audio/__init__.py
+++ bobcat/audio/__init__.py
@@ -23,5 +23,7 @@ def convert_to_mp3(input_filename, output_filename, cover_art, title):
     # disable writing encoding information as some decoders incorrectly infer VBR if it is there
     parameters = ['-write_xing','0']
 
-    audio_segment.export(output_filename, format='mp3', bitrate='128k', tags=tags, cover=cover_art, parameters=parameters)
+    audio_segment.export(output_filename, format='mp3', bitrate='128k',
+        tags=tags, cover=cover_art, parameters=parameters)
+
     logging.info('Converted %s to 128k CBR MP3 %s', input_filename, output_filename)
